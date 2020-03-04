eb788e7d8140420331e71aa9d93491019ea17f7e
Lockolocko <cmoirafi@gmail.com>
2020-03-04 11:32:39 +0100

    Print/Gestion de la classe composite

--- src/main/java/com/gillotroux/compositePersonnels.java
+++ src/main/java/com/gillotroux/compositePersonnels.java
@@ -1,5 +1,22 @@
 package com.gillotroux;
 
-public class compositePersonnels {
+import java.util.ArrayList;
+import java.util.List;
 
+public class compositePersonnels implements InterfacePersonnels{
+	private List<Personnel> listePersonnel=new ArrayList<Personnel>();
+	
+	public void add(Personnel personnel) {
+	    listePersonnel.add(personnel);
+	}
+	
+	public void remove(Personnel personnel) {
+	    listePersonnel.remove(personnel);
+	}
+	
+	public void print() {
+		for(Personnel personnel : listePersonnel) {
+			personnel.print();
+		}
+	}
 }
