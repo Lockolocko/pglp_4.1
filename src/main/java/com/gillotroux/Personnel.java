6f6d8f398efa3c454900bd091ef85e7ded9908bc
Lockolocko <cmoirafi@gmail.com>
2020-03-04 11:10:07 +0100

    Builder 3

--- src/main/java/com/gillotroux/Personnel.java
+++ src/main/java/com/gillotroux/Personnel.java
@@ -13,8 +13,11 @@ public final class Personnel {
     	// Required parameters
         nom=builder.nom;
         prenom=builder.prenom;
+        dateDeNaissance=builder.dateDeNaissance;
         
         // Optionnal parameters
+        numeroFixPro=builder.numeroFixPro;
+        numeroFixPer=builder.numeroFixPer;
     }
     
     public static class Builder{
@@ -24,14 +27,18 @@ public final class Personnel {
         private final LocalDate dateDeNaissance;
         
         // Optionnal parameters
-        private int numeroFixPro;
-        private int numeroFixPer;
+        private int numeroFixPro=0;
+        private int numeroFixPer=0;
         
-        public Builder(String nom,String prenom){
+        public Builder(String nom,String prenom,LocalDate dateDeNaissance){
             this.nom=nom;
             this.prenom=prenom;
+            this.dateDeNaissance=dateDeNaissance;
         }
         
+        public Personnel build(){
+           return new Personnel(this);
+        }
         
     }
     
