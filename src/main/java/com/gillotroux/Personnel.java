347d837e9d8a32eb0a5ee74a7456f4911d604c07
Lockolocko <cmoirafi@gmail.com>
2020-03-04 10:39:38 +0100

    Création des attributs

--- src/main/java/com/gillotroux/Personnel.java
+++ src/main/java/com/gillotroux/Personnel.java
@@ -1,5 +1,16 @@
 package com.gillotroux;
 
-public final class Personnel {
+import java.time.*;
 
+public final class Personnel {
+    private final String nom;
+    private final String prenom;
+    private final LocalDate dateDeNaissance;
+    private final int numeroFixPro;
+    private final int numeroFixPer;
+    
+    public Personnel(String nom,String prenom){
+    	this.nom=nom;
+    	this.prenom=prenom;
+    }
 }
