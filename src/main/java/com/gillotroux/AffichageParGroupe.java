ac4b63dfb8854dcbb31a2d1bde6aae7e3de3c1e0
Lockolocko <cmoirafi@gmail.com>
2020-03-11 11:20:50 +0100

    Correction checkstyle

--- src/main/java/com/gillotroux/AffichageParGroupe.java
+++ src/main/java/com/gillotroux/AffichageParGroupe.java
@@ -1,5 +1,5 @@
 /**
- * Package de Gillot-Roux
+ * Package de Gillot-Roux.
  */
 package com.gillotroux;
 
@@ -12,43 +12,69 @@ import java.util.ArrayList;
  */
 public class AffichageParGroupe {
     /**
-     * ArrayList 
+     * ArrayList.
      */
-    private ArrayList<InterfacePersonnels> Arr;
+    private ArrayList<InterfacePersonnels> arr;
+
+    /**
+     * Constructeur.
+     * @param root
+     */
+    public AffichageParGroupe(final InterfacePersonnels root) {
+        arr.add(root);
+    }
+
     /**
      * Groupe iterateur.
      * @author Lockolocko
-     *
      */
     private class GrpIterator implements Iterator {
         /**
          * Création de l'arrayList.
          */
-        public GrpIterator() {
-            Arr=new ArrayList<InterfacePersonnels>();
+        GrpIterator() {
+            int verifie = 0;
+            while (verifie < arr.size()) {
+                // "Instance of" pour savoir si c'est un composite:
+                //on ajoute tous les éléments du composite
+                // ou un Personnel: on ne fait rien
+                if (arr.iterator().next() instanceof Personnel) {
+                    Personnel p = (Personnel) arr.iterator().next();
+                    p.print();
+                } else {
+                    CompositePersonnels composite =
+(CompositePersonnels) arr.iterator().next();
+                    for (Personnel p1 : composite.getListe()) {
+                        p1.print();
+                    }
+                }
+                verifie++;
+            }
         }
-        
         /**
          * Vérifie si il y a un élément suivant.
+         * @return boolean
          */
         public boolean hasNext() {
-            if((Arr.iterator()).hasNext()) {
+            if ((arr.iterator()).hasNext()) {
                 return true;
             }
             return false;
         }
+
         /**
          * Retourne l'élément suivant.
+         * @return Personnel
          */
         public InterfacePersonnels next() {
-            if(hasNext()) {
-                InterfacePersonnels perso=Arr.iterator().next();
+            if (hasNext()) {
+                InterfacePersonnels perso = arr.iterator().next();
                 return perso;
             }
             return null;
         }
-        
     }
+
     /**
      * Donne un accès à hasNext() et next().
      * @return Iterator
