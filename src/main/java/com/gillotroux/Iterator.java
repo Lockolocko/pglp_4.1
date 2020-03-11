e78eee99fb3690026ec1f2d82c332a492c754bec
Lockolocko <cmoirafi@gmail.com>
2020-03-11 09:58:00 +0100

    Classe iterator

---
+++ src/main/java/com/gillotroux/Iterator.java
@@ -0,0 +1,14 @@
+/**
+ * Package de Gillot-Roux.
+ * @since 1.0
+ * @author Lockolocko
+ */
+package com.gillotroux;
+
+public interface Iterator<T> {
+    /**
+     * Retourne vrai si la liste contient un élément suivant.
+     * @return
+     */
+    boolean hasNext();
+}
